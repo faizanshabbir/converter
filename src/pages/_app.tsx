e347a3a099690c639c664fe5f3ba857d14cc6219
Faizan Shabbir <faizanshabbir@Faizans-MacBook-Pro.local>
2025-01-25 02:26:47 -0600

    analytics and styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" defaultTheme="system">
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ThemeProvider>
   );
